866a9dde0f5be6f7c4c2258d3ae4944e1db9aae2
daniel klein <danielklein45@gmail.com>
2016-06-11 17:14:09 +0300

    addind FilterStrategy

---
+++ B16 Ex03 Daniel 301344891 Mike 310675582/RegexFilterStrategy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FacebookSmartView
+{
+    public class RegexFilterStrategy: IFilterStrategy
+    {
+        private Regex m_Regex;
+        public RegexFilterStrategy(string i_Text)
+        {
+            m_Regex = new Regex(i_Text);
+        }
+        public bool IsMatch(string i_TextToCheck)
+        {
+            return m_Regex.Match(i_TextToCheck).Success;
+        }
+
+        public override string ToString()
+        {
+            return m_Regex.ToString();
+        }
+    }
+}
